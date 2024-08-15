429a427e32d17036a8989e2ef7397bf945666a74
whit33th <deniil019@gmail.com>
2024-08-15 17:38:46 +0200

    fix

--- myPortfolio/src/components/Navbar/Navbar.jsx
+++ myPortfolio/src/components/Navbar/Navbar.jsx
@@ -7,34 +7,46 @@ function Navbar() {
     const activeLink = 'nav-list__link nav-list__link--active'
     const defaultLink = 'nav-list__link'
 
+    function updateNavbarHeight() {
+
+        const navbar = document.querySelector('.nav')
+
+        const height = window.getComputedStyle(navbar).height
+
+        document.documentElement.style.setProperty('--navbar-height', height)
+    }
+
+    window.addEventListener('load', updateNavbarHeight)
+    window.addEventListener('resize', updateNavbarHeight)
+    
     return (
         <nav className="nav">
             <div className="container">
                 <div className="nav-row">
 
                     <NavLink to='/' className="logo">
-                        &lt;Daniil &#8260;&gt; 
+                        &lt;Daniil &#8260;&gt;
                     </NavLink>
 
-                    <DarkModeBtn/>
-                    
+                    <DarkModeBtn />
+
 
                     <ul className="nav-list">
 
 
-                        <li className="nav-list__item" ><NavLink to="/" className={({isActive}) => isActive ? activeLink : defaultLink}>
+                        <li className="nav-list__item" ><NavLink to="/" className={({ isActive }) => isActive ? activeLink : defaultLink}>
                             Home
                         </NavLink></li>
 
                         <li className="nav-list__item">
-                            <NavLink to="/projects" className={({isActive}) => isActive ? activeLink : defaultLink}>
+                            <NavLink to="/projects" className={({ isActive }) => isActive ? activeLink : defaultLink}>
                                 Projects
                             </NavLink>
 
                         </li>
 
                         <li className="nav-list__item">
-                            <NavLink to="/contacts" className={({isActive}) => isActive ? activeLink : defaultLink}>
+                            <NavLink to="/contacts" className={({ isActive }) => isActive ? activeLink : defaultLink}>
                                 Contacts
                             </NavLink>
 
